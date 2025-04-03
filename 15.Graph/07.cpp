63785e28fb738378664539a0a970e96b4e6289b0
Shubham <kuanarshubham@gmail.com>
2025-04-03 21:25:12 +0530

    2025-04-03 21:25:12

--- 15.Graph/07.cpp
+++ 15.Graph/07.cpp
@@ -4,10 +4,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int timeToRot(vector<vector<int>>g){
+void bfs(){}
+
+int timeToRot(vector<vector<int>>g, int row, int col){
     int n = g.size();
     int m = g[0].size();
     vector<vector<int>>vis(n, vector<int>(m, 0));
 
-    
+    queue<pair<int, int>>q;
+    q.push({row, col});
+    vis[row][col] = 1;
+
+    while(!q.empty()){
+        int r = q.front().first;
+        int c = q.front().second;
+
+        if(r<n-1 && !vis[r+1][c] && g[r+1][c] == 1) vis[r+1][c] = 1;
+        
+    }
 }
---
+++ 15.Graph/08.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+bool bfs(int node, vector<vector<int>>g, vector<int>&vis){
+    queue<pair<int, int>>q;
+
+    q.push({node, -1});
+    vis[node] = 1;
+
+    while(!q.empty()){
+        int currNode = q.front().first;
+        int parentNode = q.front().second;
+
+        for(auto it: g[node]){
+            
+        }
+    }
+}
+
+bool detectCycle(vector<vector<int>>g){
+    vector<int> vis(g.size(), 0);
+
+    for(int i=1; i<vis.size(); i++){
+        if(!vis[i]){
+            if(bfs(i, g, vis)){
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
