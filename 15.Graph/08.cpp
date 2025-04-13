afcd15db1a6cba3ba08372ce99b06144c38118aa
Shubham <kuanarshubham@gmail.com>
2025-04-13 18:19:47 +0530

    2025-04-13 18:19:47

--- 15.Graph/08.cpp
+++ 15.Graph/08.cpp
@@ -4,7 +4,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool bfs(int node, vector<vector<int>>g, vector<int>&vis){
+bool bfs(int node, vector<vector<int>>&g, vector<int>&vis){
     queue<pair<int, int>>q;
 
     q.push({node, -1});
@@ -13,24 +13,28 @@ bool bfs(int node, vector<vector<int>>g, vector<int>&vis){
     while(!q.empty()){
         int currNode = q.front().first;
         int parentNode = q.front().second;
+        q.pop();
 
-        for(auto it: g[node]){
-            
-
-
-            
-
-
-
-            
+        for(auto adjNode: g[currNode]){
+            if(!vis[adjNode]){
+                vis[adjNode] = 1;
+                cout<<"adjNode: "<<adjNode<<endl;
+                q.push({adjNode, currNode});
+            }
+            else if(adjNode != parentNode){
+                cout<<parentNode<<" -> "<<adjNode<<endl;
+                return true;
+            }
         }
     }
+
+    return false;
 }
 
 bool detectCycle(vector<vector<int>>g){
     vector<int> vis(g.size(), 0);
 
-    for(int i=1; i<vis.size(); i++){
+    for(int i=0; i<vis.size(); i++){
         if(!vis[i]){
             if(bfs(i, g, vis)){
                 return true;
@@ -39,4 +43,21 @@ bool detectCycle(vector<vector<int>>g){
     }
 
     return false;
+}
+
+int main(){
+    vector<vector<int>>graph = {
+        {},
+        {2, 3},
+        {1, 4},
+        {1},
+        {2},
+        {6},
+        {5},
+        {8, 9},
+        {7, 9},
+        {7, 8}
+    };
+
+    cout<<detectCycle(graph);
 }
