5e250a1216897bb34a9cf2c84a90aa21684b7d56
Shubham <kuanarshubham@gmail.com>
2025-06-20 23:52:55 +0530

    2025-06-20 23:52:55

---
+++ 16.DP/02/02.cpp
@@ -0,0 +1,19 @@
+// house robber - hosues in circular order
+
+// just take 2 arrays with on 1st arry with 0th element and 2nd array with last element
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+
+
+
+
+hksacjkljnsdlk
+;vbf hsdjkvljmvn 
+dcvbkhdbvkjdsbnj
+
+}
