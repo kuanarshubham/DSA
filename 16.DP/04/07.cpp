1fed6dbbf21e2193605cf6fe7d58442542ac7540
Shubham <kuanarshubham@gmail.com>
2025-07-24 23:49:37 +0530

    2025-07-24 23:49:36

--- 16.DP/04/07.cpp
+++ 16.DP/04/07.cpp
@@ -11,8 +11,8 @@ int number(string s1, string s2){
 
     vector<int>prev(n2+1, 0), curr(n2+1, 0);
 
-    for(int idx1=0; idx1<n1; idx1++){
-        for(int idx2=0; idx2<n2; idx2++){
+    for(int idx1=1; idx1<=n1; idx1++){
+        for(int idx2=1; idx2<=n2; idx2++){
             
             if(s1[idx1] == s2[idx2]) curr[idx2] = 1+prev[idx2-1];
             else{
@@ -30,12 +30,12 @@ int number(string s1, string s2){
     return n1+n2-lenLCS;
 }
 
-string value(string s1, string s2){
+string sequence(string s1, string s2){
     int n1=s1.size(), n2=s2.size();
     vector<vector<int>>dp(n1+1, vector<int>(n2+1, 0));
 
-    for(int idx1=0; idx1<n1; idx1++){
-        for(int idx2=0; idx2<n2; idx2++){
+    for(int idx1=1; idx1<=n1; idx1++){
+        for(int idx2=1; idx2<=n2; idx2++){
             
             if(s1[idx1] == s2[idx2]) dp[idx1][idx2] = 1+dp[idx1-1][idx2-1];
             else{
@@ -46,7 +46,43 @@ string value(string s1, string s2){
         }
     }
 
-    string ans="";
+    int lenLCS = dp[n1][n2];
 
-    for(int )
+    int lcsuper = n1+n2-lenLCS;
+    int count = lcsuper-1;
+
+    string ans(lcsuper, '$');
+
+    int idx1=n1, idx2=n2;
+    while(idx1>0 && idx2>0){
+        if(s1[idx1-1]==s2[idx2-1]){
+            ans[count]=s1[idx1-1];
+            count--;
+            idx1--;
+            idx2--;
+        }
+        else{
+            if(dp[idx1-1][idx2]>dp[idx1][idx2-1]){
+                ans[count]=s1[idx1];
+                count--;
+                idx1--;
+            }
+            else{
+                ans[count]=s1[idx1];
+                count--;
+                idx2--;
+            }
+        }
+    }
+
+    while (idx1 > 0) {
+        ans += s1[idx1 - 1];
+        idx1--;
+    }
+    while (idx2 > 0) {
+        ans += s2[idx2 - 1];
+        idx2--;
+    }
+
+    return ans;
 }
