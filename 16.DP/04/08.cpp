84eb71008767441a38cfaaebdfefce3132949dd9
Shubham <kuanarshubham@gmail.com>
2025-07-24 23:52:07 +0530

    2025-07-24 23:52:07

--- 16.DP/04/08.cpp
+++ 16.DP/04/08.cpp
@@ -1,1 +1,11 @@
-// 
+// how many times does seq 2 comes in seq 1
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int memo(string s1, string s2, int idx1, int idx2){
+    
+
+    
+}
