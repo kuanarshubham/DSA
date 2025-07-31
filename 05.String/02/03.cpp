e7e49c3c3afc52adb2a579a44969d64217f5078c
Shubham <kuanarshubham@gmail.com>
2025-07-31 23:45:14 +0530

    2025-07-31 23:45:14

--- 05.String/02/03.cpp
+++ 05.String/02/03.cpp
@@ -30,30 +30,42 @@ int toInt(string s){
 }
 
 string toRoman(int n){
-    vector<pair<int, char>>intToRoman = {
-        {1, 'I'},
-        {4, 'IV'},
-        {5, 'V'},
-        {9, 'IX'},
-        {10, 'X'},
-        {40, 'XL'},
-        {50, 'L'},
-        {90, 'XC'},
-        {100, 'C'},
-        {400, 'CD'},
-        {500, 'D'},
-        {900, 'CM'},
-        {1000, 'M'}       
+    vector<pair<int, string>> intToRoman = {
+        {1, "I"},
+        {4, "IV"},
+        {5, "V"},
+        {9, "IX"},
+        {10, "X"},
+        {40, "XL"},
+        {50, "L"},
+        {90, "XC"},
+        {100, "C"},
+        {400, "CD"},
+        {500, "D"},
+        {900, "CM"},
+        {1000, "M"}       
     };
 
-    int i=12;
+    int i=12; 
+    string ans="";
 
-    while(n){
-        int div = n/
+    while(i){
+        int div = intToRoman[i].first;
+        string value = intToRoman[i].second;
+        int divisor = n/div;
+        if(divisor>0){
+            for(int i=1; i<=divisor; i++) ans+=value;
+
+            n%=div;
+        }
+        i--;
     }
+
+    return ans;
 }
 
 int main(){
     string s = "MCMXCIV";
-    cout<<toInt(s);
+    cout<<toInt(s)<<endl;
+    cout<<toRoman(1994);
 }
