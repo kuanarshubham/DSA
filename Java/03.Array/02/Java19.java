f34eae432d0da29f200384be26bd29f33d4de99c
Shubham <kuanarshubham@gmail.com>
2025-09-26 00:06:49 +0530

    2025-09-26 00:06:49

--- Java/03.Array/02/Java19.java
+++ Java/03.Array/02/Java19.java
@@ -4,15 +4,19 @@ public class Java19 {
     public static int buySellStock1(int[]days){
         int n=days.length;
 
-        int 
+        int miniDay=Integer.MAX_VALUE, maxiProf=Integer.MIN_VALUE;
 
         for(int i=0; i<n; i++){
-
+            miniDay = Integer.min(miniDay, days[i]);
+            maxiProf = Integer.max(maxiProf, -miniDay+days[i]);
         }
+
+        return maxiProf;
     }
+
     public static void main(String[] args) {
-        int[] days = new int[]{};
+        int[] days = new int[]{7,1,5,3,6,4};
 
-
+        System.err.println(buySellStock1(days));
     }
 }
---
+++ Java/03.Array/02/Java20.java
@@ -0,0 +1,16 @@
+// alternative positive and negative
+
+public class Java20 {
+    public static void alternativePosNeg(int[]nums){
+        int n=nums.length;
+
+        int pos=-1, neg=-1;
+
+        for(int i=0; i<n; i++){
+            
+        }
+    } 
+    public static void main(String[] args) {
+        
+    }
+}
