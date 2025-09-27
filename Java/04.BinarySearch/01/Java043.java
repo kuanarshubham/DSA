29d38cc9dd15ff5336da4f123557067ad6c2e05d
Shubham <kuanarshubham@gmail.com>
2025-09-27 17:29:30 +0530

    2025-09-27 17:29:29

--- Java/04.BinarySearch/01/Java043.java
+++ Java/04.BinarySearch/01/Java043.java
@@ -6,23 +6,43 @@ public class Java043 {
     public static int ceil(int[]nums, int target){
         int n=nums.length;
 
-        int low=0, high=n-1, ub=n;
+        int low=0, high=n-1, ans=n;
 
         while(low<=high){
             int mid = low + (high-low)/2;
 
-            if(nums[mid]>target){
-                ub=mid;
+            if(nums[mid]>=target){
+                ans=mid;
                 high=mid-1;
             }
             else low=mid+1;
         }
 
-        return ub;
+        return ans;
+    }
+
+    public static int floor(int[]nums, int target){
+        int n=nums.length;
+
+        int low=0, high=n-1, ans=-1;
+
+        while(low<=high){
+            int mid = low + (high-low)/2;
+
+            if(nums[mid]<=target){
+                ans=mid;
+                low=mid+1;
+            }
+            else high=mid-1;
+        }
+
+        return ans;
     }
 
 
     public static void main(String[] args) {
-        
+        int[]nums = {4, 7, 9, 12, 14, 72};
+
+        System.err.println(floor(nums, 13) + " " + ceil(nums, 13));
     }
 }
---
+++ Java/04.BinarySearch/01/Java044.java
@@ -0,0 +1,7 @@
+// first and last occurance
+
+public class Java044 {
+    public static void main(String[] args) {
+        
+    }
+}
