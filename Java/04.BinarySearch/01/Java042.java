3b4332eeacdb3d04358ee65439d2d980203033e0
Shubham <kuanarshubham@gmail.com>
2025-09-27 17:14:43 +0530

    2025-09-27 17:14:43

--- Java/04.BinarySearch/01/Java042.java
+++ Java/04.BinarySearch/01/Java042.java
@@ -1,5 +1,24 @@
 public class Java042 {
+    public static int lowerBound(int[]nums, int toInsert){
+        int n=nums.length;
+
+        int low=0, high=n-1, lb=n;
+
+        while(low<=high){
+            int mid = low + (high-low)/2;
+
+            if(nums[mid]>=toInsert){
+                lb=mid;
+                high=mid-1;
+            }
+            else low=mid+1;
+        }
+
+        return lb;
+    }
     public static void main(String[] args) {
-        
+        int[] nums = new int[]{1,2,4,7};
+
+        System.err.println(lowerBound(nums, 6));
     }
 }
