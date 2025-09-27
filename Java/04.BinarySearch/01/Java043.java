0e6f0536d3f6eaa6ed60b7f44277e18ada01a331
Shubham <kuanarshubham@gmail.com>
2025-09-27 17:19:40 +0530

    2025-09-27 17:19:40

--- Java/04.BinarySearch/01/Java043.java
+++ Java/04.BinarySearch/01/Java043.java
@@ -1,3 +1,28 @@
+// floor/ceil in sorted array
+
+// just use the lower-bound and upper-bound
+
 public class Java043 {
-    
+    public static int ceil(int[]nums, int target){
+        int n=nums.length;
+
+        int low=0, high=n-1, ub=n;
+
+        while(low<=high){
+            int mid = low + (high-low)/2;
+
+            if(nums[mid]>target){
+                ub=mid;
+                high=mid-1;
+            }
+            else low=mid+1;
+        }
+
+        return ub;
+    }
+
+
+    public static void main(String[] args) {
+        
+    }
 }
