7535dd932fd3c37145f21373f3882be012aa2d26
Shubham <kuanarshubham@gmail.com>
2025-09-28 23:53:44 +0530

    2025-09-28 23:53:44

---
+++ Java/04.BinarySearch/02/Java055.java
@@ -0,0 +1,17 @@
+// smallest divisor
+
+public class Java055 {
+    public static int value
+    public static int smallestDivisor(int[] num, int limit){
+        int low=2, high=num;
+
+        while(low<=high){
+            int mid = low+(high-low)/2;
+
+
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
