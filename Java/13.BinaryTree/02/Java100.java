168ab2ff1dfdbd7c20916ea7b6f2375396f59ed5
Shubham <kuanarshubham@gmail.com>
2025-09-30 23:59:39 +0530

    2025-09-30 23:59:39

--- Java/13.BinaryTree/02/Java100.java
+++ Java/13.BinaryTree/02/Java100.java
@@ -1,1 +1,23 @@
-//
+// maximum path sum
+
+class Node {
+    public int data;
+    public Node left;
+    public Node right;
+
+    public Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    public Node(int data, Node left, Node right) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Java100 {
+    static 
+}
