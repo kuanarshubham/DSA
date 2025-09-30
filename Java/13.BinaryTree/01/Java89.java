3392c869c6336c341f0d30afbe74399d24699f50
Shubham <kuanarshubham@gmail.com>
2025-09-30 14:18:25 +0530

    2025-09-30 14:18:24

--- Java/13.BinaryTree/01/Java89.java
+++ Java/13.BinaryTree/01/Java89.java
@@ -1,1 +1,39 @@
-//
+// preorder
+
+import java.util.ArrayList;
+
+class Node{
+    public int data;
+    public Node left;
+    public Node right;
+
+    public Node(int data){
+        this.data=data;
+        this.left=null;
+        this.right=null;
+    }
+
+    public Node(int data, Node left, Node right){
+        this.data=data;
+        this.left=left;
+        this.right=right;
+    }
+}
+
+public class Java89 {
+    static void helper(Node root, ArrayList<Integer>tarversal){
+        if(root==null) return;
+
+        tarversal.add(root.data);
+        helper(root.left, tarversal);
+        helper(root.right, tarversal);
+    }
+
+    static ArrayList<Integer> preorder(Node root){
+        ArrayList<Integer>tarversal = new ArrayList<>();
+
+        helper(root, tarversal);
+
+        return tarversal;
+    }
+}
---
+++ Java/13.BinaryTree/01/Java90.java
@@ -0,0 +1,22 @@
+// inorder
+
+class Node{
+    public int data;
+    public Node left;
+    public Node right;
+
+    public Node(int data){
+        this.data=data;
+        this.left=null;
+        this.right=null;
+    }
+
+    public Node(int data, Node left, Node right){
+        this.data=data;
+        this.left=left;
+        this.right=right;
+    }
+}
+
+public class Java90 {
+}
