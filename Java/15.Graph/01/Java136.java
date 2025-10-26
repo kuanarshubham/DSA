92fb70363e982d7b5ed70671702d48070321d3eb
Shubham <kuanarshubham@gmail.com>
2025-10-26 14:57:46 +0530

    2025-10-26 14:57:45

---
+++ Java/15.Graph/01/Java136.java
@@ -0,0 +1,10 @@
+// represtion
+
+public class Java136 {
+    public static void main(String[] args) {
+        int n = 10;
+        int[][] graph = new int[n+1][n+1];
+
+        
+    }
+}
--- Java/15.Graph/01/Onejava.java
+++
@@ -1,5 +0,0 @@
-public class Onejava(){
-    public static void main(String[] args) {
-        
-    }
-}
